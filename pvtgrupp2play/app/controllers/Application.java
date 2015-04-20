4d27a454fbbf2ca8ee82bb8bfde5d4c346c6714a
Nicklas Andersson <nicklas@wwn.se>
2015-04-20 14:34:00 +0200

    db

--- pvtgrupp2play/app/controllers/Application.java
+++ pvtgrupp2play/app/controllers/Application.java
@@ -3,17 +3,19 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import java.sql.*;
+import java.sql.DriverManager;
 
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
+
 		Connection conn = null;
 		Statement stmt = null;
 		String result = "";
 		try{
-			conn = DB.getConnection();
+			conn = DriverManager.getConnection("mysql://mysql.dsv.su.se/nian8516", "nian8516", "ozaezithaibe");
 			stmt = conn.createStatement();
 			String sql = "SELECT * FROM Fred_Test";
 			ResultSet rs = stmt.executeQuery(sql);
