e6ed9a3e14df71032f29d0a4f38789abc11ee12d
Nicklas Andersson <nicklas@wwn.se>
2015-04-20 14:16:56 +0200

    test db

--- pvtgrupp2play/app/controllers/Application.java
+++ pvtgrupp2play/app/controllers/Application.java
@@ -8,7 +8,23 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+		Connection conn = null;
+		Statement stmt = null;
+		String result = "";
+		try{
+			conn = DB.getConnection();
+			stmt = conn.createStatement();
+			String sql = "SELECT * FROM Fred_Test";
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				result = result + rs;
+			}
+		}catch(Exeption e){
+			
+		}
+		
+		
+        return ok(index.render("Your new application is ready." + rs));
     }
 
 }
