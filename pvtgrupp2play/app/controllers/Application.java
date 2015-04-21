8b88f3fc1e54baa1238db96f040340695679ba5e
Nicklas Andersson <nicklas@wwn.se>
2015-04-21 09:38:56 +0200

    Stavning

--- pvtgrupp2play/app/controllers/Application.java
+++ pvtgrupp2play/app/controllers/Application.java
@@ -2,6 +2,8 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+
+import java.lang.Exception;
 import java.sql.*;
 import java.sql.DriverManager;
 
@@ -14,18 +16,18 @@ public class Application extends Controller {
 		Connection conn = null;
 		Statement stmt = null;
 		String result = "";
-		try{
-			conn = DriverManager.getConnection("mysql://mysql.dsv.su.se/nian8516", "nian8516", "ozaezithaibe");
-			stmt = conn.createStatement();
-			String sql = "SELECT * FROM Fred_Test";
-			ResultSet rs = stmt.executeQuery(sql);
-			while(rs.next()){
-				result = result + rs;
-			}
-		}catch(Exeption e){
-			
-		}
-		
+        try {
+            conn = DriverManager.getConnection("mysql://mysql.dsv.su.se/nian8516", "nian8516", "ozaezithaibe");
+            stmt = conn.createStatement();
+            String sql = "SELECT * FROM Fred_Test";
+            ResultSet rs = stmt.executeQuery(sql);
+        }catch(Exception e){
+            return ok(index.render(e));
+        }
+        while(rs.next()){
+            result = result + rs;
+        }
+
         return ok(index.render("Your new application is ready." + result));
     }
 
