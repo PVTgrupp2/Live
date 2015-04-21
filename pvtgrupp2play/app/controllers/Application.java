04a09e16ccb5a6b55a29fbd32081ee1ac5d80014
Nicklas Andersson <nicklas@wwn.se>
2015-04-21 10:55:51 +0200

    Snyggartill lite

--- pvtgrupp2play/app/controllers/Application.java
+++ pvtgrupp2play/app/controllers/Application.java
@@ -15,20 +15,20 @@ public class Application extends Controller {
 
 		Connection conn = null;
 		Statement stmt = null;
-		String result = "\n";
+		String result = "";
         try {
             conn = DriverManager.getConnection("jdbc:mysql://mysql.dsv.su.se/nian8516", "nian8516", "ozaezithaibe");
             stmt = conn.createStatement();
             String sql = "SELECT * FROM Fred_Test";
             ResultSet rs = stmt.executeQuery(sql);			
 			while(rs.next()){
-				result = result + rs.getString(1) + rs.getString(2) + rs.getString(3);
+				result = result + rs.getString(1) + rs.getString(2) + rs.getString(3) + "<br>";
 			}
         }catch(Exception e){
             return ok(index.render(e.toString()));
         }
         
-        return ok(index.render("Your new application is ready.\n" + result));
+        return ok(index.render("Your new application is ready.<br>" + result));
     }
 
 }
