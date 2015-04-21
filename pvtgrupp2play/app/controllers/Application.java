593b8a3bc93c8e3dcc977e4d898a8c6d5e6da36f
Nicklas Andersson <nicklas@wwn.se>
2015-04-21 10:38:42 +0200

    kanske nu :santa:

--- pvtgrupp2play/app/controllers/Application.java
+++ pvtgrupp2play/app/controllers/Application.java
@@ -17,7 +17,7 @@ public class Application extends Controller {
 		Statement stmt = null;
 		String result = "";
         try {
-            conn = getConnection();
+            conn = DriverManager.getConnection("mysql.dsv.su.se/nian8516", "nian8516", "ozaezithaibe");
             stmt = conn.createStatement();
             String sql = "SELECT * FROM Fred_Test";
             ResultSet rs = stmt.executeQuery(sql);			
@@ -29,10 +29,6 @@ public class Application extends Controller {
         }
         
         return ok(index.render("Your new application is ready.\n" + result));
-    }
-
-    private Connection getConnection() throws URISyntaxException, SQLException {
-        return DriverManager.getConnection("mysql.dsv.su.se/nian8516", "nian8516", "ozaezithaibe");
     }
 
 }
