37fbb305f332ea0c2e225c9469f07d70f9ce66cd
Nicklas Andersson <nicklas@wwn.se>
2015-04-23 12:52:03 +0200

    Allow cross orign

--- pvtgrupp2play/app/controllers/Application.java
+++ pvtgrupp2play/app/controllers/Application.java
@@ -71,6 +71,16 @@ public class Application extends Controller {
             return ok(json);
         }
     }
+    
+    
+    //Allow cross orgin
+    public static Result preflight(String all) {
+        response().setHeader("Access-Control-Allow-Origin", "*");
+        response().setHeader("Allow", "*");
+        response().setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+        response().setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent");
+        return ok();
+    }
 
 
 
