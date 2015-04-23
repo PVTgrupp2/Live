2d4ad15695c46c5275d449df50daa98ee74ca676
Nicklas Andersson <nicklas@wwn.se>
2015-04-23 13:06:37 +0200

    More header

--- pvtgrupp2play/app/controllers/Application.java
+++ pvtgrupp2play/app/controllers/Application.java
@@ -64,11 +64,15 @@ public class Application extends Controller {
     }
     
     public static Result fbauth(){
+        response().setHeader("Access-Control-Allow-Origin", "*");
+        response().setHeader("Allow", "*");
+        response().setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+        response().setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent");
+        
         com.fasterxml.jackson.databind.JsonNode json = request().body().asJson();
         if(json == null){
             return badRequest("Expecting Json data");
         }else{
-            response().setHeader("Access-Control-Allow-Origin", "*");
             return ok(json);
         }
     }
