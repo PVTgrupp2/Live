6c4a2c281b8f77074179d4de71c81b3b9b5c9ea0
Alsar5 <freddepersson@gmail.com>
2015-04-24 12:39:29 +0200

    Fixar

--- web/includes.js
+++ web/includes.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
 	$('footer').html("\
+				<div class='ui-footer ui-bar-a ui-footer-fixed slideup' role='contentinfo' data-role='footer' data-position='fixed' data-theme='a'>\
+				<div role='navigation' class='ui-navbar' data-role='navbar'>\
+				<ul class='ui-grid-c'>\
+				<li class='ui-block-a'><a class='ui-btn' href='top10.html' data-prefetch='true' data-transition='fade'>Toplista</a></li>\
+				<li class='ui-block-b'><a class='ui-btn ui-btn-active' href='map.html' data-prefetch='true' data-transition='fade'>Karta</a></li>\
+				<li class='ui-block-c'><a class='ui-btn' href='list.html' data-prefetch='true' data-transition='fade'>Sök</a></li>\
+				<li class='ui-block-d'><a class='ui-btn' href='mypage.html' data-prefetch='true' data-transition='fade'>Min Sida</a></li>\
+				</ul></div></div>");
 });
