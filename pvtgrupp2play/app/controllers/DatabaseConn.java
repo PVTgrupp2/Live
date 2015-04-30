8822cb3c809ccafe960269ccc4b28a6517475f73
Nicklas Andersson <nicklas@wwn.se>
2015-04-30 14:06:30 +0200

    Dont store pass in git

--- pvtgrupp2play/app/controllers/DatabaseConn.java
+++ pvtgrupp2play/app/controllers/DatabaseConn.java
@@ -4,6 +4,7 @@ import java.sql.*;
 
 class DatabaseConn{
     public static Connection getConn() throws Exception{
-        return DriverManager.getConnection("jdbc:mysql://mysql.dsv.su.se/nian8516", "nian8516", "ozaezithaibe");
+        String pass = System.getenv("DBpass");
+        return DriverManager.getConnection("jdbc:mysql://mysql.dsv.su.se/nian8516", "nian8516", pass);
     }
 }
