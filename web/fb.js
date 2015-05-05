aeb2854be9fed98a32063b262b8cd5cafff6123c
Sarrgas <sargas1@hotmail.com>
2015-05-05 15:12:32 +0200

    Hide/show

--- web/fb.js
+++ web/fb.js
@@ -27,13 +27,19 @@ function checkLogin(){
 	    var uid = response.authResponse.userID;
 	    var accessToken = response.authResponse.accessToken;
 	    console.log("Inloggad");
+        $('#login').hide();
+        $('#minsida').show();
 	  } else if (response.status === 'not_authorized') {
 	    // the user is logged in to Facebook, 
 	    // but has not authenticated your app
 	    console.log("Offline");
+        $('#login').show();
+        $('#minsida').hide();
 	  } else {
 	    // the user isn't logged in to Facebook.
 	    console.log("Offline");
+        $('#login').show();
+        $('#minsida').hide();
 	  }
 	});
 }
