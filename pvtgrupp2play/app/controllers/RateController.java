5168e3eaeccf617e034b0800145ec5bf9002b64d
Nicklas Andersson <nicklas@wwn.se>
2015-05-11 12:25:10 +0200

    cors fix

--- pvtgrupp2play/app/controllers/RateController.java
+++ pvtgrupp2play/app/controllers/RateController.java
@@ -17,6 +17,11 @@ package controllers;
  
  public class RateController extends Controller{
      public static Result rate(){
+        response().setHeader("Access-Control-Allow-Origin", "*");
+        response().setHeader("Allow", "*");
+        response().setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+        response().setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent");
+         
         com.fasterxml.jackson.databind.JsonNode json = request().body().asJson();
         if(json == null){
             return badRequest("Expecting Json data");
