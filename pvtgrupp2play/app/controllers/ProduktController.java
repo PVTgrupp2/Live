1a66bb99796d15fb11db427e6027baf0cb00f518
Nicklas Andersson <nicklas@wwn.se>
2015-05-13 09:55:33 +0200

    cors uts

--- pvtgrupp2play/app/controllers/ProduktController.java
+++ pvtgrupp2play/app/controllers/ProduktController.java
@@ -17,6 +17,11 @@
  
  class ProduktController extends Controller{
      public static Result getAll() {
+        response().setHeader("Access-Control-Allow-Origin", "*");
+        response().setHeader("Allow", "*");
+        response().setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+        response().setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent");
+        
         ArrayList<Produkt> thelist = new ArrayList<>();
         
         try{
