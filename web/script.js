07dff85e6381b953e65cd1bc854879a294833aa7
Sarrgas <sargas1@hotmail.com>
2015-05-18 14:00:22 +0200

    Du måste logga in för blablabla

--- web/script.js
+++ web/script.js
@@ -2,8 +2,5 @@ $(document).ready(function(){
 	$('#popAvbryt').click(function(){
 		$('#p10000').popup('close');
 	});
-	$('#popRate').click(function(){
-		console.log("Bedöm biatch");
-		window.location.href="#rate";
-	});
+	
 });
