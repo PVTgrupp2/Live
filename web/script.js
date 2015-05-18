6aa92edd827eedd1cf64768f44e1ff1381043886
Sarrgas <sargas1@hotmail.com>
2015-05-18 13:20:52 +0200

    Popup bedöm

--- web/script.js
+++ web/script.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
 	$('#popAvbryt').click(function(){
 		$('#p10000').popup('close');
 	});
+	$('#popRate').click(function(){
+		console.log("Bedöm biatch");
+		window.location.href="#rate";
+	});
 });
