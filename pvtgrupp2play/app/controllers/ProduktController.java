012aebb017f6f4460a521d88011a23aed28d6b53
Nicklas Andersson <nicklas@wwn.se>
2015-05-18 15:16:17 +0200

    Fixa toplista istället för alla

--- pvtgrupp2play/app/controllers/ProduktController.java
+++ pvtgrupp2play/app/controllers/ProduktController.java
@@ -138,6 +138,123 @@
         }
     }
     
+    public static Result getTopBeer() {
+        response().setHeader("Access-Control-Allow-Origin", "*");
+        response().setHeader("Allow", "*");
+        response().setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+        response().setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent");
+        ArrayList<Produkt> thelist = new ArrayList<>();
+        
+        try{
+		    Connection conn = DatabaseConn.getConn();
+    		Statement stmt = null;
+            stmt = conn.createStatement();
+            
+            String sql = "SELECT * FROM nian8516.Produkt_Betyg WHERE Kategori=1 ORDER BY Betyg DESC LIMIT 15";
+            
+            ResultSet rs = stmt.executeQuery(sql);	
+            
+    		while(rs.next()){
+    		    Produkt p = new Produkt();
+    		    p.id = rs.getString("idProdukt");
+    		    p.namn = rs.getString("Namn");
+    		    p.land = rs.getString("Land");
+    			String temp = rs.getString("Betyg"); 
+				//Kontroller?
+				p.betyg = temp;
+    		    thelist.add(p);
+    		}
+    		conn.close();
+        }catch(Exception e){
+		    return internalServerError("Oops: Database Error" + e.toString());
+        }
+        
+        //TODO more controlls?
+        if(thelist.isEmpty()){
+            return internalServerError("Oops: the beers is on the table");
+        }else{
+            return ok(list.render(thelist));
+        }
+    }
+    
+    public static Result getTopWisky() {
+        response().setHeader("Access-Control-Allow-Origin", "*");
+        response().setHeader("Allow", "*");
+        response().setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+        response().setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent");
+        ArrayList<Produkt> thelist = new ArrayList<>();
+        
+        try{
+		    Connection conn = DatabaseConn.getConn();
+    		Statement stmt = null;
+            stmt = conn.createStatement();
+            
+            String sql = "SELECT * FROM nian8516.Produkt_Betyg WHERE Kategori=2 ORDER BY Betyg DESC LIMIT 15";
+            
+            ResultSet rs = stmt.executeQuery(sql);	
+            
+    		while(rs.next()){
+    		    Produkt p = new Produkt();
+    		    p.id = rs.getString("idProdukt");
+    		    p.namn = rs.getString("Namn");
+    		    p.land = rs.getString("Land");
+    			String temp = rs.getString("Betyg"); 
+				//Kontroller?
+				p.betyg = temp;
+    		    thelist.add(p);
+    		}
+    		conn.close();
+        }catch(Exception e){
+		    return internalServerError("Oops: Database Error" + e.toString());
+        }
+        
+        //TODO more controlls?
+        if(thelist.isEmpty()){
+            return internalServerError("Oops: the beers is on the table");
+        }else{
+            return ok(list.render(thelist));
+        }
+    }
+    
+        public static Result getTop() {
+        response().setHeader("Access-Control-Allow-Origin", "*");
+        response().setHeader("Allow", "*");
+        response().setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+        response().setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent");
+        ArrayList<Produkt> thelist = new ArrayList<>();
+        
+        try{
+		    Connection conn = DatabaseConn.getConn();
+    		Statement stmt = null;
+            stmt = conn.createStatement();
+            
+            String sql = "SELECT * FROM nian8516.Produkt_Betyg ORDER BY Betyg DESC LIMIT 15";
+            
+            ResultSet rs = stmt.executeQuery(sql);	
+            
+    		while(rs.next()){
+    		    Produkt p = new Produkt();
+    		    p.id = rs.getString("idProdukt");
+    		    p.namn = rs.getString("Namn");
+    		    p.land = rs.getString("Land");
+    			String temp = rs.getString("Betyg"); 
+				//Kontroller?
+				p.betyg = temp;
+    		    thelist.add(p);
+    		}
+    		conn.close();
+        }catch(Exception e){
+		    return internalServerError("Oops: Database Error" + e.toString());
+        }
+        
+        //TODO more controlls?
+        if(thelist.isEmpty()){
+            return internalServerError("Oops: the beers is on the table");
+        }else{
+            return ok(list.render(thelist));
+        }
+    }
+    
     public static Result get(Long id){
         response().setHeader("Access-Control-Allow-Origin", "*");
         response().setHeader("Allow", "*");
