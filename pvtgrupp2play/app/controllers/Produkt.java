397912440aaaae8b17555bae971560203f786b5c
lisafinne <lisafinne@hotmail.com>
2015-05-19 14:35:32 +0200

    betygtest

--- pvtgrupp2play/app/controllers/Produkt.java
+++ pvtgrupp2play/app/controllers/Produkt.java
@@ -2,13 +2,42 @@ package controllers;
 public class Produkt{
     public String id;
     public String namn;
-    public String betyg;
+    public int betyg;
     public String land;
     public String beskr;
     public String alkhalt;
     
     
-    Produkt(){
+    public Produkt(){
         
     }
+    
+    public int getBetyg(){
+        
+        return betyg;
+        
+    }
+    
+    
+    public void setBetyg(String betyginput){
+        
+    Integer betygnr;
+       
+    if (betyginput!=null){
+       
+    if (!betyginput.isEmpty()){
+            try{
+                betygnr=Integer.parseInt(betyginput);
+                if (betygnr>=1&&betygnr<=5){
+                    this.betyg=betygnr.intValue();
+                }
+                
+            }catch (NumberFormatException e){
+                e.printStackTrace();
+            }
+        }
+    } 
+    }
+    
 }
+
