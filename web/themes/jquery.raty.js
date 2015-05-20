53ceee73ffd18d399d25f59d59e7e1c441925857
Alsar5 <freddepersson@gmail.com>
2015-05-20 11:45:35 +0200

    Fixade stödnamnen på ratingen

--- web/themes/jquery.raty.js
+++ web/themes/jquery.raty.js
@@ -730,7 +730,7 @@
     click        : undefined,
     half         : false,
     halfShow     : true,
-    hints        : ['bad', 'poor', 'regular', 'good', 'gorgeous'],
+    hints        : ['Vidrig', 'Godkänd', 'Bra', 'Mycket bra', 'Mästerverk'],
     iconRange    : undefined,
     mouseout     : undefined,
     mouseover    : undefined,
