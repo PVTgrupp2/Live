e5083ead3ae0d015ac97864bbc5114463471d849
Nicklas Andersson <nicklas@wwn.se>
2015-05-20 11:30:30 +0200

    setToTaste

--- pvtgrupp2play/app/controllers/ToTaste.java
+++ pvtgrupp2play/app/controllers/ToTaste.java
@@ -16,7 +16,50 @@
  
  public class ToTaste extends Controller{
      
-     public static Result getToTaste(Long id) {
+     public static Result setToTaste(){
+        ObjectNode result = Json.newObject();
+        response().setHeader("Access-Control-Allow-Origin", "*");
+        response().setHeader("Allow", "*");
+        response().setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+        response().setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent");
+         
+        com.fasterxml.jackson.databind.JsonNode json = request().body().asJson();
+        if(json == null){
+            result.put("status", "Expecting Json data");
+        }else{
+            String uid = json.findPath("uid").textValue();
+            String pid = json.findPath("pid").textValue();
+            //String score = json.findPath("score").textValue();
+            String token = json.findPath("token").textValue();
+            
+            if(uid.length() != 18 && pid.length() != 5 && token == null){
+                result.put("status", "Missing parameter");
+            }else{
+                if(true){ //if(FBvalidator.validateFb(token)){
+                    
+                    try{
+                        Connection conn = DatabaseConn.getConn();
+            		    Statement stmt = null;
+                        stmt = conn.createStatement();
+                        String sql = "INSERT INTO nian8516.1ToTaste (1idanv, idprodukt) VALUES('"+ uid +"','" + pid + "')" +
+                         "ON DUPLICATE KEY UPDATE 1idanv='" + uid + "', idprodukt='" + pid + "'";
+                        stmt.executeUpdate(sql);	
+                    }catch(Exception e){
+                        result.put("status", e.toString());
+                    }
+                        
+                    //return ok(result);
+                    
+                }else{
+                    result.put("status", "Login fail");
+                }
+            }
+        }
+        return ok(result);
+    }
+     
+     
+    public static Result getToTaste(Long id) {
         response().setHeader("Access-Control-Allow-Origin", "*");
         response().setHeader("Allow", "*");
         response().setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
