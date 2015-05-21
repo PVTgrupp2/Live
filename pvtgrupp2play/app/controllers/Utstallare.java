a3004d7cc896b4057e88cd389b90abb2b31d1de8
lisafinne <lisafinne@hotmail.com>
2015-05-21 11:49:56 +0200

    uttällaresetters

--- pvtgrupp2play/app/controllers/Utstallare.java
+++ pvtgrupp2play/app/controllers/Utstallare.java
@@ -1,14 +1,66 @@
 package controllers;
 public class Utstallare{
-    public String id;
+    public int id;
     public String namn;
-    public String betyg;
     public String land;
     public String beskr;
-    public String alkhalt;
-    
     
     Utstallare(){
         
     }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public void setId(String idstr) throws Exception {
+        
+        Integer idnr;
+        
+        if (idstr!=null){
+            if (!idstr.isEmpty()){
+                try {
+                    idnr = Integer.parseInt(idstr);
+                    this.id=idnr.intValue();
+                }catch (NumberFormatException e){
+                    e.printStackTrace();
+                }
+            }
+        }else {
+            throw new Exception("Inget id");
+        }
+    }
+    
+    public String getNamn(){
+        return namn;
+    }
+    
+      public void setNamn(String namn){
+        if (namn!=null){
+            if (!namn.isEmpty()&&namn.length()<=45&&namn.length()>1){
+                this.namn=namn;
+        }
+    }else this.namn=new String("Inget namn");
+    
+    }
+    
+    
+    public String getLand(){
+        return land;
+    }
+  
+    public void setLand(String land){
+        if (land!=null){
+            if (!land.isEmpty()&&land.length()<=45&&land.length()>1){
+                this.namn=namn;
+        }
+    }else this.land=new String("Inget land");
+  
+    }
+    
+    
+    
+    
+    
+    
 }
