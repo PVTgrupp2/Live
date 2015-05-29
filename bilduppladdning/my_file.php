92110b77bad00cb912abc3c969ded8528283268b
berkevall <berkevall@gmail.com>
2015-05-29 12:52:37 +0200

    MY_FILE.php design klar(?)

--- bilduppladdning/my_file.php
+++ bilduppladdning/my_file.php
@@ -4,18 +4,31 @@
 	.custom-file-upload {
     border: 1px solid #e7c0bb;
     display: inline-block;
-    padding: 6px 12px;
+    /*padding: 7px 15px;*/
+    padding: 7px 0px;
     cursor: pointer;
     color: #b03120;
-
+    font-size: 45px;
+    width: 300px;
+    
 	}
 	input[type="file"] {
     display: none;
+
 	}
 
 	.style{
-		text-align: center;
-
+	text-align: center;
+	top: 35%;
+	position: relative;
+	}
+	input.substyle{	
+    width: 300px;
+    height: 50px;
+    font-size: 30px;
+    color: #b03120;
+    border: 1px solid #e7c0bb;
+    background-color: #fff;
 	}
 	</style>
 </head>
@@ -25,9 +38,9 @@
 <label for="picture" class="custom-file-upload">
     Välj bild
 </label>
-<input type="file" capture="camera" accept="image/*" id="picture" name="uploadedfile">
+<input type="file" capture="camera" accept="image/*" id="picture" name="uploadedfile" >
 <br /><br />
-<input type="submit" value="Ladda upp bild"/>
+<input type="submit" class="substyle" value="Ladda upp bilden"/>
 </form>
 </div>
 </html>
