3b7ca52b61b5d6a4a0dcef6763468c9c62baaf90
berkevall <berkevall@gmail.com>
2015-05-29 11:40:19 +0200

    test

--- bilduppladdning/my_file.php
+++ bilduppladdning/my_file.php
@@ -2,17 +2,25 @@
 <head>
 	<style>
 	.custom-file-upload {
-    border: 1px solid #ccc;
+    border: 1px solid #e7c0bb;
     display: inline-block;
     padding: 6px 12px;
     cursor: pointer;
+    color: #b03120;
+
 	}
 	input[type="file"] {
     display: none;
 	}
+
+	.style{
+		text-align: center;
+
+	}
 	</style>
 </head>
 
+<div class="style">
 <form enctype="multipart/form-data" method="post" action="upload.php">
 <label for="picture" class="custom-file-upload">
     Välj bild
@@ -21,4 +29,5 @@
 <br /><br />
 <input type="submit" value="Ladda upp bild"/>
 </form>
+</div>
 </html>
